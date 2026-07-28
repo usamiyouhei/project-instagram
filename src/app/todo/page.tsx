3f397fa87b1f209d19e7aa7dc5c5ed4a194efbfc
youhei usami <usao9110@icloud.com>
2026-07-28 10:03:22 +0400

    Update page.tsx

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -3,6 +3,8 @@
 import React, { useState } from "react";
 import styles from "./todo.module.scss";
 import { Todo } from "@/types/todo";
+import TodoForm from "@/components/todo/TodoForm";
+import TodoList from "@/components/todo/TodoList";
 
 export default function Home() {
   const [todos, setTodos] = useState<Todo[]>([]);
@@ -16,9 +18,46 @@ export default function Home() {
 
     setTodos((prevTodos) => [...prevTodos, newTodo]);
   };
+
+  const toggleTodo = (id: string) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id
+          ? {
+              ...todo,
+              completed: !todo.completed,
+            }
+          : todo,
+      ),
+    );
+  };
+
+  const deleteTodo = (id: string) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  const inCompletedCount = todos.filter((todo) => !todo.completed).length;
+
   return (
-    <main>
-      <h1>Todo Apps</h1>
+    <main className={styles.page}>
+      <section className={styles.todoCard}>
+        <div className={styles.heading}>
+          <p className={styles.label}>Next.js Mini Project</p>
+          <h1 className={styles.title}>Todo Apps</h1>
+          <p className={styles.description}>
+            Organize your tasks and keep moving forward.
+          </p>
+        </div>
+
+        <TodoForm onAdd={addTodo} />
+
+        <div className={styles.status}>
+          <span>{inCompletedCount}tasks remaning</span>
+          <span>total</span>
+        </div>
+
+        <TodoList todos={todos} onToggle={toggleTodo} onDelete={deleteTodo} />
+      </section>
     </main>
   );
 }
