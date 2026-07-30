6d1fa6ab87b0bf7823ad8fcbcc0004639f8939b6
youhei usami <usao9110@icloud.com>
2026-07-30 10:55:19 +0400

    update todoList

--- src/components/todo/TodoItem.tsx
+++ src/components/todo/TodoItem.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import styles from "./TodoItem.module.scss";
 import { Todo } from "@/types/todo";
+import { Pencil, Trash2 } from "lucide-react";
 
 type TodoItemProps = {
   todo: Todo;
   onToggle: (id: string) => void;
   onDelete: (id: string) => void;
+  onEdit: (id: string) => void;
 };
 
-export default function TodoItem({ todo, onToggle, onDelete }: TodoItemProps) {
+export default function TodoItem({
+  todo,
+  onToggle,
+  onDelete,
+  onEdit,
+}: TodoItemProps) {
   return (
     <li className={styles.item}>
       <label className={styles.content}>
@@ -18,27 +25,40 @@ export default function TodoItem({ todo, onToggle, onDelete }: TodoItemProps) {
           checked={todo.completed}
           onChange={() => onToggle(todo.id)}
         />
-        <span
-          className={`${styles.text} ${todo.completed ? styles.completed : ""}`}
-        >
-          {todo.text}
-        </span>
-        <time
-          className={`${styles.time} ${todo.completed ? styles.completed : ""}`}
-          dateTime={todo.createdAt}
-        >
-          {new Date(todo.createdAt).toLocaleString("ja-JP")}
-        </time>
+        <div className={styles.todoInfo}>
+          <span
+            className={`${styles.text} ${todo.completed ? styles.completed : ""}`}
+          >
+            {todo.text}
+          </span>
+          <time
+            className={`${styles.time} ${todo.completed ? styles.completed : ""}`}
+            dateTime={todo.createdAt}
+          >
+            {new Date(todo.createdAt).toLocaleString("ja-JP")}
+          </time>
+        </div>
       </label>
 
-      <button
-        className={styles.deleteButton}
-        type="button"
-        onClick={() => onDelete(todo.id)}
-        aria-label={`Delete ${todo.id}`}
-      >
-        Delete
-      </button>
+      <div className={styles.actions}>
+        <button
+          className={styles.iconButton}
+          type="button"
+          onChange={() => onEdit(todo.id)}
+          aria-label={`edit ${todo.text}`}
+        >
+          <Pencil size={20} strokeWidth={1.8} />
+        </button>
+
+        <button
+          className={styles.iconButton}
+          type="button"
+          onClick={() => onDelete(todo.id)}
+          aria-label={`delete ${todo.text}`}
+        >
+          <Trash2 size={20} strokeWidth={1.8} />
+        </button>
+      </div>
     </li>
   );
 }
