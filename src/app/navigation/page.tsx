dc404e4072eebaace9065441e0d620bf3354e466
youhei usami <usao9110@icloud.com>
2026-09-02 09:23:26 +0400

    update FloatingNav

--- src/app/navigation/page.tsx
+++ src/app/navigation/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./page.module.scss";
-import FloatingNavbar from "@/components/ui/navigation/FloatingNavbar";
+import LiquidGlassNavbar from "@/components/ui/navigation/LiquidGlassNavbar/LiquidGlassNavbar";
+import FloatingNavbar from "@/components/ui/navigation/FloatingNavbar/FloatingNavbar";
 
 export default function page() {
   return (
@@ -8,12 +9,18 @@ export default function page() {
       <div className={styles.content}>
         <p className={styles.label}>UI COMPONENT 02</p>
 
-        <h1>Navigation bar</h1>
+        <div className={styles.floating}>
+          <h1>Navigation bar 1</h1>
 
-        <p className={styles.discription}>
-          Move your cursor over the navigation button.
-        </p>
-        <FloatingNavbar />
+          <p className={styles.description}>
+            Move your cursor over the navigation button.
+          </p>
+          <FloatingNavbar />
+        </div>
+
+        <div className={styles.liquid}>
+          <LiquidGlassNavbar />
+        </div>
       </div>
     </main>
   );
--- src/components/ui/navigation/LiquidGlassNavbar/LiquidGlassNavbar.tsx
+++ src/components/ui/navigation/LiquidGlassNavbar/LiquidGlassNavbar.tsx
@@ -2,6 +2,26 @@
 import React from "react";
 import styles from "./LiquidGlassNavbar.module.scss";
 
+const navItems = ["Home", "Work", "About", "Contact"];
+
 export default function LiquidGlassNavbar() {
-  return <div></div>;
+  return (
+    <nav className={styles.navigation}>
+      <div className={styles.glassLayer}>
+        <div className={styles.content}>
+          <a href="#" className={styles.logo}>
+            USAMI
+          </a>
+
+          <div className={styles.menu}>
+            {navItems.map((item) => (
+              <a key={item} href="#">
+                {item}
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
